c9b44fd4cd5d155c469c40d9b71776563cd126e1
Chris Lozon <doverboy29@gmail.com>
2022-09-14 09:20:42 -0400

    working well ui is lacking

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 let numberOfRounds = 0
+let winningScore = 0
 let playerScore = 0
 let computerScore = 0
-let playersSelection = ''
+let playerSelection = ''
+const winningMessage = "Congratulation's!! You Won!!"
+const losingMessage = "Sorry... You lost!!"
 
 const options = document.querySelectorAll('.weapon-selection')
 const roundSelection = document.querySelectorAll('.round-selection')
-//const startButton = document.getElementById('button')
-const container = document.getElementById('container')
-const rounds = document.getElementById('rounds')
-const roundsContainer = document.getElementById('rounds-container')
+const container = document.querySelector('#container')
+const rounds = document.querySelector('#rounds')
+const roundsContainer = document.querySelector('#rounds-container')
+const playerTally = document.querySelector('#player-score')
+const computerTally = document.querySelector('#computer-score')
+const scoreContainer = document.querySelector('#score-container')
+const verdict = document.querySelector('#verdict')
+
 const weapons = {
   Rock: {
     strongAgainst: 'Scissors'
@@ -20,26 +27,22 @@ const weapons = {
     strongAgainst: 'Paper'
   }
 }
-/*
-startButton.addEventListener('click', function(event) {
-  numberOfRounds = rounds.value
-  console.log(numberOfRounds)
-  container.style.display = 'block'
-  roundsContainer.style.display = 'none'
-})
-*/
+//eventListeners for players choice of rounds
 for (const round of roundSelection) {
   round.addEventListener('click', function(event) {
     numberOfRounds = event.target.id
+    winningScore = Math.ceil(numberOfRounds/2)
     container.style.display = 'block'
+    scoreContainer.style.display = 'block'
     roundsContainer.style.display = 'none'
   })
 }
-
+//eventListeners for the players weapon choice
 for (const option of options) {
   option.addEventListener('click', function (event) {
-    playersChoice = event.target.id
-    console.log(playersChoice)
+    playerSelection = event.target.id
+
+    game()
   })
 }
 //get a randomized computer selection returning 1 of the 3 options
@@ -49,40 +52,60 @@ function getComputerSelection() {
   let selection = answers[randomize]
   return selection
 }
-//compare both selections returning an alert with message about the results and increasing winners score by 1
+//displays message for players round verdict
+function roundVerdict (message) {
+  if(numberOfRounds == 1) return
+  verdict.innerHTML = message
+  setTimeout(() => {
+    verdict.innerHTML = ''
+  }, 1000)
+}
+//compare both selections returning a message about the results and increasing winners score by 1
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return alert("It's a Tie!!")
+    let message = "It's a Tie"
+    roundVerdict(message)
   } else if (weapons[playerSelection].strongAgainst === computerSelection) {
     playerScore++
-    return alert(`You Win! ${playerSelection} beats ${computerSelection}`)
+    playerTally.innerHTML = playerScore
+    let message = `You Win! ${playerSelection} beats ${computerSelection}`
+    roundVerdict(message)
   } else {
     computerScore++
-    return alert(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    computerTally.innerHTML = computerScore
+    let message = `You Lose! ${computerSelection} beats ${playerSelection}`
+    roundVerdict(message)
   }
 }
-
-//runs the playRound function 5 times and returning an alert on the results of the winner
-function game() {
-  for (i = 1; i <= 5; i++) {
-    let playerSelection = getPlayerSelection()
-    let computerSelection = getComputerSelection()
-    validAnswer = false
-
-    playRound(playerSelection, computerSelection)
-
-    if (playerScore === 3) {
-      return alert("Congratulation's!! You Won!!")
-    } else if (computerScore === 3) {
-      return alert('Sorry!! You lost!!')
-    }
+//displays final verdict with a delay to print message out 1 letter at a time
+function finalVerdict(verdictMessage) {
+  for (let i = 0; i < verdictMessage.length; i++) {
+    setTimeout(() => {
+        verdict.innerHTML += verdictMessage[i];
+    }, i * 100);
   }
+}
+//runs the playRound function 5 times and returning a message on the results of the winner
+function game() {
+  let computerSelection = getComputerSelection()
 
-  if (playerScore === computerScore) {
-    return alert('Round Over. Game is tied!!')
-  } else if (playerScore > computerScore) {
-    return alert(`Congratulation's!! You Won ${playerScore} - ${computerScore}`)
-  } else if (playerScore < computerScore) {
-    return alert(`Sorry!! You lost ${computerScore} - ${playerScore}`)
+  playRound(playerSelection, computerSelection)
+
+  if(numberOfRounds == 1) {
+    container.style.display = 'none'
+    if(playerScore > computerScore) {
+      finalVerdict(winningMessage)
+    } else if(computerScore > playerScore){
+      finalVerdict(losingMessage)
+    }
+  } else if(playerScore == winningScore) {
+    container.style.display = 'none'
+    finalVerdict(winningMessage)
+  } else if(computerScore == winningScore) {
+    container.style.display = 'none'
+    finalVerdict(losingMessage)
+  } else {
+    numberOfRounds--
+    playerSelection = ''
   }
 }
